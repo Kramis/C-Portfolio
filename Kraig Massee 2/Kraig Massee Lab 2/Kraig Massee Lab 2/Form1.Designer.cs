27bc9bd40fe5592edad6e2bc75d466017b470118
Kramis <47329654+Kramis@users.noreply.github.com>
2019-06-03 09:42:46 -0700

    Add files via upload

---
+++ Kraig Massee 2/Kraig Massee Lab 2/Kraig Massee Lab 2/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace Kraig_Massee_Lab_2
+{
+    partial class pictureBoxForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(pictureBoxForm));
+            this.mainPictureBoxLabel = new System.Windows.Forms.Label();
+            this.onePictureBox = new System.Windows.Forms.PictureBox();
+            this.twoPictureBox = new System.Windows.Forms.PictureBox();
+            this.threePictureBox = new System.Windows.Forms.PictureBox();
+            this.fivePictureBox = new System.Windows.Forms.PictureBox();
+            this.fourPictureBox = new System.Windows.Forms.PictureBox();
+            this.onePictureBoxLabel = new System.Windows.Forms.Label();
+            this.twoPictureBoxLabel = new System.Windows.Forms.Label();
+            this.threePictureBoxLabel = new System.Windows.Forms.Label();
+            this.fourPictureBoxLabel = new System.Windows.Forms.Label();
+            this.fivePictureBoxLabel = new System.Windows.Forms.Label();
+            this.controlToolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.onePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.twoPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.threePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fivePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fourPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainPictureBoxLabel
+            // 
+            this.mainPictureBoxLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.mainPictureBoxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mainPictureBoxLabel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.mainPictureBoxLabel.Location = new System.Drawing.Point(294, 46);
+            this.mainPictureBoxLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.mainPictureBoxLabel.Name = "mainPictureBoxLabel";
+            this.mainPictureBoxLabel.Size = new System.Drawing.Size(297, 39);
+            this.mainPictureBoxLabel.TabIndex = 0;
+            this.mainPictureBoxLabel.Text = "Picture Box";
+            this.mainPictureBoxLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // onePictureBox
+            // 
+            this.onePictureBox.Image = ((System.Drawing.Image)(resources.GetObject("onePictureBox.Image")));
+            this.onePictureBox.Location = new System.Drawing.Point(59, 127);
+            this.onePictureBox.Margin = new System.Windows.Forms.Padding(2);
+            this.onePictureBox.Name = "onePictureBox";
+            this.onePictureBox.Size = new System.Drawing.Size(150, 300);
+            this.onePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.onePictureBox.TabIndex = 1;
+            this.onePictureBox.TabStop = false;
+            this.controlToolTip.SetToolTip(this.onePictureBox, "One");
+            this.onePictureBox.Click += new System.EventHandler(this.onePictureBox_Click);
+            // 
+            // twoPictureBox
+            // 
+            this.twoPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("twoPictureBox.Image")));
+            this.twoPictureBox.Location = new System.Drawing.Point(213, 127);
+            this.twoPictureBox.Margin = new System.Windows.Forms.Padding(2);
+            this.twoPictureBox.Name = "twoPictureBox";
+            this.twoPictureBox.Size = new System.Drawing.Size(150, 300);
+            this.twoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.twoPictureBox.TabIndex = 2;
+            this.twoPictureBox.TabStop = false;
+            this.controlToolTip.SetToolTip(this.twoPictureBox, "Two");
+            this.twoPictureBox.Click += new System.EventHandler(this.twoPictureBox_Click);
+            // 
+            // threePictureBox
+            // 
+            this.threePictureBox.Image = ((System.Drawing.Image)(resources.GetObject("threePictureBox.Image")));
+            this.threePictureBox.Location = new System.Drawing.Point(367, 127);
+            this.threePictureBox.Margin = new System.Windows.Forms.Padding(2);
+            this.threePictureBox.Name = "threePictureBox";
+            this.threePictureBox.Size = new System.Drawing.Size(150, 300);
+            this.threePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.threePictureBox.TabIndex = 3;
+            this.threePictureBox.TabStop = false;
+            this.controlToolTip.SetToolTip(this.threePictureBox, "Three");
+            this.threePictureBox.Click += new System.EventHandler(this.threePictureBox_Click);
+            // 
+            // fivePictureBox
+            // 
+            this.fivePictureBox.Image = ((System.Drawing.Image)(resources.GetObject("fivePictureBox.Image")));
+            this.fivePictureBox.Location = new System.Drawing.Point(675, 127);
+            this.fivePictureBox.Margin = new System.Windows.Forms.Padding(2);
+            this.fivePictureBox.Name = "fivePictureBox";
+            this.fivePictureBox.Size = new System.Drawing.Size(150, 300);
+            this.fivePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.fivePictureBox.TabIndex = 4;
+            this.fivePictureBox.TabStop = false;
+            this.controlToolTip.SetToolTip(this.fivePictureBox, "Five");
+            this.fivePictureBox.Click += new System.EventHandler(this.fivePictureBox_Click);
+            // 
+            // fourPictureBox
+            // 
+            this.fourPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("fourPictureBox.Image")));
+            this.fourPictureBox.Location = new System.Drawing.Point(521, 127);
+            this.fourPictureBox.Margin = new System.Windows.Forms.Padding(2);
+            this.fourPictureBox.Name = "fourPictureBox";
+            this.fourPictureBox.Size = new System.Drawing.Size(150, 300);
+            this.fourPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.fourPictureBox.TabIndex = 5;
+            this.fourPictureBox.TabStop = false;
+            this.controlToolTip.SetToolTip(this.fourPictureBox, "Four");
+            this.fourPictureBox.Click += new System.EventHandler(this.fourPictureBox_Click);
+            // 
+            // onePictureBoxLabel
+            // 
+            this.onePictureBoxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.onePictureBoxLabel.Location = new System.Drawing.Point(59, 427);
+            this.onePictureBoxLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.onePictureBoxLabel.Name = "onePictureBoxLabel";
+            this.onePictureBoxLabel.Size = new System.Drawing.Size(150, 36);
+            this.onePictureBoxLabel.TabIndex = 8;
+            this.onePictureBoxLabel.Text = "One";
+            this.onePictureBoxLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.onePictureBoxLabel.Visible = false;
+            this.onePictureBoxLabel.Click += new System.EventHandler(this.onePictureBoxLabel_Click);
+            // 
+            // twoPictureBoxLabel
+            // 
+            this.twoPictureBoxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.twoPictureBoxLabel.Location = new System.Drawing.Point(213, 427);
+            this.twoPictureBoxLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.twoPictureBoxLabel.Name = "twoPictureBoxLabel";
+            this.twoPictureBoxLabel.Size = new System.Drawing.Size(150, 36);
+            this.twoPictureBoxLabel.TabIndex = 9;
+            this.twoPictureBoxLabel.Text = "Two";
+            this.twoPictureBoxLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.twoPictureBoxLabel.Visible = false;
+            this.twoPictureBoxLabel.Click += new System.EventHandler(this.twoPictureBoxLabel_Click);
+            // 
+            // threePictureBoxLabel
+            // 
+            this.threePictureBoxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.threePictureBoxLabel.Location = new System.Drawing.Point(367, 429);
+            this.threePictureBoxLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.threePictureBoxLabel.Name = "threePictureBoxLabel";
+            this.threePictureBoxLabel.Size = new System.Drawing.Size(150, 36);
+            this.threePictureBoxLabel.TabIndex = 10;
+            this.threePictureBoxLabel.Text = "Three";
+            this.threePictureBoxLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.threePictureBoxLabel.Visible = false;
+            this.threePictureBoxLabel.Click += new System.EventHandler(this.threePictureBoxLabel_Click);
+            // 
+            // fourPictureBoxLabel
+            // 
+            this.fourPictureBoxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fourPictureBoxLabel.Location = new System.Drawing.Point(521, 427);
+            this.fourPictureBoxLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.fourPictureBoxLabel.Name = "fourPictureBoxLabel";
+            this.fourPictureBoxLabel.Size = new System.Drawing.Size(150, 36);
+            this.fourPictureBoxLabel.TabIndex = 11;
+            this.fourPictureBoxLabel.Text = "Four";
+            this.fourPictureBoxLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.fourPictureBoxLabel.Visible = false;
+            this.fourPictureBoxLabel.Click += new System.EventHandler(this.fourPictureBoxLabel_Click);
+            // 
+            // fivePictureBoxLabel
+            // 
+            this.fivePictureBoxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fivePictureBoxLabel.Location = new System.Drawing.Point(675, 427);
+            this.fivePictureBoxLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.fivePictureBoxLabel.Name = "fivePictureBoxLabel";
+            this.fivePictureBoxLabel.Size = new System.Drawing.Size(150, 36);
+            this.fivePictureBoxLabel.TabIndex = 12;
+            this.fivePictureBoxLabel.Text = "Five";
+            this.fivePictureBoxLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.fivePictureBoxLabel.Visible = false;
+            this.fivePictureBoxLabel.Click += new System.EventHandler(this.fivePictureBoxLabel_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(213, 487);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(2);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(100, 52);
+            this.clearButton.TabIndex = 6;
+            this.clearButton.Text = "&Clear";
+            this.controlToolTip.SetToolTip(this.clearButton, "This button clears the pictures and the picture labels");
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(571, 487);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(2);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(100, 52);
+            this.exitButton.TabIndex = 7;
+            this.exitButton.Text = "e&Xit";
+            this.controlToolTip.SetToolTip(this.exitButton, "This button exits the program");
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBoxForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(884, 582);
+            this.Controls.Add(this.fivePictureBoxLabel);
+            this.Controls.Add(this.fourPictureBoxLabel);
+            this.Controls.Add(this.threePictureBoxLabel);
+            this.Controls.Add(this.twoPictureBoxLabel);
+            this.Controls.Add(this.onePictureBoxLabel);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.fourPictureBox);
+            this.Controls.Add(this.fivePictureBox);
+            this.Controls.Add(this.threePictureBox);
+            this.Controls.Add(this.twoPictureBox);
+            this.Controls.Add(this.onePictureBox);
+            this.Controls.Add(this.mainPictureBoxLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "pictureBoxForm";
+            this.Text = "Picture Box";
+            this.Load += new System.EventHandler(this.pictureBoxForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.onePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.twoPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.threePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fivePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fourPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label mainPictureBoxLabel;
+        private System.Windows.Forms.PictureBox onePictureBox;
+        private System.Windows.Forms.PictureBox twoPictureBox;
+        private System.Windows.Forms.PictureBox threePictureBox;
+        private System.Windows.Forms.PictureBox fivePictureBox;
+        private System.Windows.Forms.PictureBox fourPictureBox;
+        private System.Windows.Forms.Label onePictureBoxLabel;
+        private System.Windows.Forms.Label twoPictureBoxLabel;
+        private System.Windows.Forms.Label threePictureBoxLabel;
+        private System.Windows.Forms.Label fourPictureBoxLabel;
+        private System.Windows.Forms.Label fivePictureBoxLabel;
+        private System.Windows.Forms.ToolTip controlToolTip;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Kraig Massee 2/Kraig Massee Lab 2/Kraig Massee Lab 2/Form1.cs
@@ -0,0 +1,112 @@
+﻿//Program: Picture Box
+//Date: January 2019
+//Author: Kraig Massee
+
+//Displays a form with multiple picture boxes and controls to manipulate picture boxes and labels. Contains a clear button to clear
+//the picture boxes and labels and an exit button to exit the program
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace Kraig_Massee_Lab_2
+{
+    public partial class pictureBoxForm : Form
+    {
+        public pictureBoxForm()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBoxForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //When the eXit button is click this will show a Message box and then close the program
+            MessageBox.Show("The Program will be terminated");
+            this.Close();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            //When the Clear button is clicked this method clears all picture box images and the labels associated with them
+            onePictureBox.Image = null;
+            twoPictureBox.Image = null;
+            threePictureBox.Image = null;
+            fourPictureBox.Image = null;
+            fivePictureBox.Image = null;
+            onePictureBoxLabel.Text = null;
+            twoPictureBoxLabel.Text = null;
+            threePictureBoxLabel.Text = null;
+            fourPictureBoxLabel.Text = null;
+            fivePictureBoxLabel.Text = null;
+        }
+
+        private void onePictureBoxLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void twoPictureBoxLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void threePictureBoxLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fourPictureBoxLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fivePictureBoxLabel_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void onePictureBox_Click(object sender, EventArgs e)
+        {
+            //This sets the onePictureBoxLabel to visible on click of the onePictureBox
+            onePictureBoxLabel.Visible = true;
+        }
+
+        private void twoPictureBox_Click(object sender, EventArgs e)
+        {
+            //This sets the twoPictureBoxLabel to visible on click of the twoPictureBox
+            twoPictureBoxLabel.Visible = true;
+        }
+
+        private void threePictureBox_Click(object sender, EventArgs e)
+        {
+            //This sets the threePictureBoxLabel to visible on click of the threePictureBox
+            threePictureBoxLabel.Visible = true;
+        }
+
+        private void fourPictureBox_Click(object sender, EventArgs e)
+        {
+            //This sets the fourPictureBoxLabel to visible on click of the fourPictureBox
+            fourPictureBoxLabel.Visible = true;
+        }
+
+        private void fivePictureBox_Click(object sender, EventArgs e)
+        {
+            //This sets the fivePictureBoxLabel to visible on click of the fivePictureBox
+            fivePictureBoxLabel.Visible = true;
+        }
+    }
+}
